acd99cefc319cd06dc7b94d6b56ad58f248be00f
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2017-07-10 09:37:10 -0400

    New tests

--- pybgen/tests/test_pybgen.py
+++ pybgen/tests/test_pybgen.py
@@ -32,11 +32,10 @@ import numpy as np
 from pkg_resources import resource_filename
 
 from .. import pybgen
-from .truths import example_32bits_truths as truths_1
-from .truths import cohort1_truths as truths_2
+from .truths import truths
 
 
-class TestPyBGEN_1(unittest.TestCase):
+class _TestPyBGEN(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -48,6 +47,9 @@ class TestPyBGEN_1(unittest.TestCase):
             __name__,
             os.path.join("data", "example.32bits.bgen"),
         )
+
+        # Getting the truth for this file
+        cls.truths = truths["example.32bits.truths.txt.bz2"]
 
     def setUp(self):
         # Reading the BGEN file
@@ -74,22 +76,25 @@ class TestPyBGEN_1(unittest.TestCase):
         """Tests the __repr__ representation."""
         self.assertEqual(
             "PyBGEN({:,d} samples; {:,d} variants)".format(
-                truths_1["nb_samples"], truths_1["nb_variants"],
+                self.truths["nb_samples"], self.truths["nb_variants"],
             ),
             str(self.bgen),
         )
 
     def test_nb_samples(self):
         """Tests the number of samples."""
-        self.assertEqual(truths_1["nb_samples"], self.bgen.nb_samples)
+        self.assertEqual(self.truths["nb_samples"], self.bgen.nb_samples)
 
     def test_nb_variants(self):
         """Tests the number of variants."""
-        self.assertEqual(truths_1["nb_variants"], self.bgen.nb_variants)
+        self.assertEqual(self.truths["nb_variants"], self.bgen.nb_variants)
 
     def test_samples(self):
         """Tests the samples attribute."""
-        self.assertEqual(truths_1["samples"], self.bgen.samples)
+        if self.truths["samples"] is None:
+            self.assertTrue(self.bgen.samples is None)
+        else:
+            self.assertEqual(self.truths["samples"], self.bgen.samples)
 
     def test_get_first_variant(self):
         """Tests getting the first variant of the file."""
@@ -102,11 +107,14 @@ class TestPyBGEN_1(unittest.TestCase):
         variant, dosage = r.pop()
 
         # Checking the variant
-        self._compare_variant(truths_1["variants"][name]["variant"], variant)
+        self._compare_variant(
+            self.truths["variants"][name]["variant"],
+            variant,
+        )
 
         # Checking the dosage
         np.testing.assert_array_almost_equal(
-            truths_1["variants"][name]["dosage"], dosage,
+            self.truths["variants"][name]["dosage"], dosage,
         )
 
     def test_get_middle_variant(self):
@@ -120,11 +128,14 @@ class TestPyBGEN_1(unittest.TestCase):
         variant, dosage = r.pop()
 
         # Checking the variant
-        self._compare_variant(truths_1["variants"][name]["variant"], variant)
+        self._compare_variant(
+            self.truths["variants"][name]["variant"],
+            variant,
+        )
 
         # Checking the dosage
         np.testing.assert_array_almost_equal(
-            truths_1["variants"][name]["dosage"], dosage,
+            self.truths["variants"][name]["dosage"], dosage,
         )
 
     def test_get_last_variant(self):
@@ -138,11 +149,14 @@ class TestPyBGEN_1(unittest.TestCase):
         variant, dosage = r.pop()
 
         # Checking the variant
-        self._compare_variant(truths_1["variants"][name]["variant"], variant)
+        self._compare_variant(
+            self.truths["variants"][name]["variant"],
+            variant,
+        )
 
         # Checking the dosage
         np.testing.assert_array_almost_equal(
-            truths_1["variants"][name]["dosage"], dosage,
+            self.truths["variants"][name]["dosage"], dosage,
         )
 
     def test_get_missing_variant(self):
@@ -164,25 +178,40 @@ class TestPyBGEN_1(unittest.TestCase):
 
             # Comparing the variant
             self._compare_variant(
-                truths_1["variants"][name]["variant"],
+                self.truths["variants"][name]["variant"],
                 variant,
             )
 
             # Comparing the dosage
             np.testing.assert_array_almost_equal(
-                truths_1["variants"][name]["dosage"], dosage,
+                self.truths["variants"][name]["dosage"], dosage,
             )
 
         # Checking if we checked all variants
-        self.assertEqual(seen_variants, truths_1["variant_set"])
+        self.assertEqual(seen_variants, self.truths["variant_set"])
 
 
-class TestPyBGEN_2(unittest.TestCase):
+class Test_1(_TestPyBGEN):
 
     @classmethod
     def setUpClass(cls):
-        # Creating a temporary directory
-        cls.tmp_dir = mkdtemp(prefix="pybgen_test_")
+        super(Test_1, cls).setUpClass()
+
+        # Getting the BGEN file
+        cls.bgen_fn = resource_filename(
+            __name__,
+            os.path.join("data", "example.32bits.bgen"),
+        )
+
+        # Getting the truth for this file
+        cls.truths = truths["example.32bits.truths.txt.bz2"]
+
+
+class Test_2(_TestPyBGEN):
+
+    @classmethod
+    def setUpClass(cls):
+        super(Test_2, cls).setUpClass()
 
         # Getting the BGEN file
         cls.bgen_fn = resource_filename(
@@ -190,129 +219,37 @@ class TestPyBGEN_2(unittest.TestCase):
             os.path.join("data", "cohort1.bgen"),
         )
 
-    def setUp(self):
-        # Reading the BGEN file
-        self.bgen = pybgen.PyBGEN(self.bgen_fn)
+        # Getting the truth for this file
+        cls.truths = truths["cohort1.truths.txt.bz2"]
+
+
+class Test_3(_TestPyBGEN):
 
     @classmethod
-    def tearDownClass(cls):
-        # Cleaning the temporary directory
-        shutil.rmtree(cls.tmp_dir)
+    def setUpClass(cls):
+        super(Test_3, cls).setUpClass()
 
-    def tearDown(self):
-        # Closing the object
-        self.bgen.close()
-
-    def _compare_variant(self, expected, observed):
-        """Compare two variants."""
-        self.assertEqual(expected.name, observed.name)
-        self.assertEqual(expected.chrom, observed.chrom)
-        self.assertEqual(expected.pos, observed.pos)
-        self.assertEqual(expected.a1, observed.a1)
-        self.assertEqual(expected.a2, observed.a2)
-
-    def test_repr(self):
-        """Tests the __repr__ representation."""
-        self.assertEqual(
-            "PyBGEN({:,d} samples; {:,d} variants)".format(
-                truths_2["nb_samples"], truths_2["nb_variants"],
-            ),
-            str(self.bgen),
+        # Getting the BGEN file
+        cls.bgen_fn = resource_filename(
+            __name__,
+            os.path.join("data", "example.24bits.bgen"),
         )
 
-    def test_nb_samples(self):
-        """Tests the number of samples."""
-        self.assertEqual(truths_2["nb_samples"], self.bgen.nb_samples)
+        # Getting the truth for this file
+        cls.truths = truths["example.24bits.truths.txt.bz2"]
 
-    def test_nb_variants(self):
-        """Tests the number of variants."""
-        self.assertEqual(truths_2["nb_variants"], self.bgen.nb_variants)
 
-    def test_samples(self):
-        """Tests the samples attribute."""
-        self.assertTrue(self.bgen.samples is None)
+class Test_4(_TestPyBGEN):
 
-    def test_get_first_variant(self):
-        """Tests getting the first variant of the file."""
-        # The variant to retrieve
-        name = "RSID_1"
+    @classmethod
+    def setUpClass(cls):
+        super(Test_4, cls).setUpClass()
 
-        # Getting the results (there should be only one
-        r = self.bgen.get_variant(name)
-        self.assertEqual(1, len(r))
-        variant, dosage = r.pop()
-
-        # Checking the variant
-        self._compare_variant(truths_2["variants"][name]["variant"], variant)
-
-        # Checking the dosage
-        np.testing.assert_array_almost_equal(
-            truths_2["variants"][name]["dosage"], dosage,
+        # Getting the BGEN file
+        cls.bgen_fn = resource_filename(
+            __name__,
+            os.path.join("data", "example.3bits.bgen"),
         )
 
-    def test_get_middle_variant(self):
-        """Tests getting a variant in the middle of the file."""
-        # The variant to retrieve
-        name = "RSID_161"
-
-        # Getting the results (there should be only one
-        r = self.bgen.get_variant(name)
-        self.assertEqual(1, len(r))
-        variant, dosage = r.pop()
-
-        # Checking the variant
-        self._compare_variant(truths_2["variants"][name]["variant"], variant)
-
-        # Checking the dosage
-        np.testing.assert_array_almost_equal(
-            truths_2["variants"][name]["dosage"], dosage,
-        )
-
-    def test_get_last_variant(self):
-        """Tests getting the last variant of the file."""
-        # The variant to retrieve
-        name = "RSID_200"
-
-        # Getting the results (there should be only one
-        r = self.bgen.get_variant(name)
-        self.assertEqual(1, len(r))
-        variant, dosage = r.pop()
-
-        # Checking the variant
-        self._compare_variant(truths_2["variants"][name]["variant"], variant)
-
-        # Checking the dosage
-        np.testing.assert_array_almost_equal(
-            truths_2["variants"][name]["dosage"], dosage,
-        )
-
-    def test_get_missing_variant(self):
-        """Tests getting a variant which is absent from the BGEN file."""
-        with self.assertRaises(ValueError) as cm:
-            self.bgen.get_variant("UNKOWN_VARIANT_NAME")
-        self.assertEqual(
-            "UNKOWN_VARIANT_NAME: name not found",
-            str(cm.exception),
-        )
-
-    def test_iter_all_variants(self):
-        """Tests the iteration of all variants."""
-        seen_variants = set()
-        for variant, dosage in self.bgen.iter_variants():
-            # The name of the variant
-            name = variant.name
-            seen_variants.add(name)
-
-            # Comparing the variant
-            self._compare_variant(
-                truths_2["variants"][name]["variant"],
-                variant,
-            )
-
-            # Comparing the dosage
-            np.testing.assert_array_almost_equal(
-                truths_2["variants"][name]["dosage"], dosage,
-            )
-
-        # Checking if we checked all variants
-        self.assertEqual(seen_variants, truths_2["variant_set"])
+        # Getting the truth for this file
+        cls.truths = truths["example.3bits.truths.txt.bz2"]
--- pybgen/tests/truths.py
+++ pybgen/tests/truths.py
@@ -37,69 +37,62 @@ __copyright__ = "Copyright 2014 Louis-Philippe Lemieux Perreault"
 __license__ = "MIT"
 
 
-__all__ = ["example_32bits_truths"]
+__all__ = ["truths"]
 
 
-# Creating the truth value for the 'example.32bits.bgen' file
-fn = resource_filename(
-    __name__, os.path.join("data", "example.32bits.truths.txt.bz2"),
+_FILENAMES = (
+    os.path.join("data", "example.3bits.truths.txt.bz2"),
+    os.path.join("data", "example.32bits.truths.txt.bz2"),
+    os.path.join("data", "example.24bits.truths.txt.bz2"),
+    os.path.join("data", "cohort1.truths.txt.bz2"),
 )
-example_32bits_truths = {}
-with bz2.BZ2File(fn, "r") as f:
-    # Reading the data
-    data = tuple(
-        row.strip().split("\t") for row in f.read().decode().splitlines()
-    )
-
-    # The first row contains the samples
-    samples = tuple(data[0])
-    example_32bits_truths["samples"] = samples
-    example_32bits_truths["nb_samples"] = len(samples)
-
-    # The first column of each row contains the markers
-    markers = tuple(row[0] for row in data[1:])
-    example_32bits_truths["variant_set"] = set(markers)
-    example_32bits_truths["nb_variants"] = len(markers)
-
-    # Now, constructing the variants
-    example_32bits_truths["variants"] = {}
-    for variant_data in data[1:]:
-        name, chrom, pos, a1, a2 = variant_data[:5]
-        variant = Variant(name, chrom, int(pos), a1, a2)
-        dosage = np.array(variant_data[5:], dtype=float)
-        example_32bits_truths["variants"][name] = {
-            "variant": variant,
-            "dosage": dosage,
-        }
 
 
-# Creating the truth value for the 'cohort1.bgen' file
-fn = resource_filename(
-    __name__, os.path.join("data", "cohort1.truths.txt.bz2"),
-)
-cohort1_truths = {}
-with bz2.BZ2File(fn, "r") as f:
-    # Reading the data
-    data = tuple(
-        row.strip().split("\t") for row in f.read().decode().splitlines()
-    )
+def _generate_truths():
+    """Generate the truths."""
+    final_truths = {}
 
-    # This file contains no sample
-    cohort1_truths["samples"] = None
-    cohort1_truths["nb_samples"] = 500
+    # The "example" file name
+    for filename in _FILENAMES:
+        fn = resource_filename(__name__, filename)
+        file_truths = {}
+        with bz2.BZ2File(fn, "r") as f:
+            # Reading the data
+            data = tuple(
+                row.strip().split("\t")
+                for row in f.read().decode().splitlines()
+            )
 
-    # The first column of each row contains the markers
-    markers = tuple(row[0] for row in data[1:])
-    cohort1_truths["variant_set"] = set(markers)
-    cohort1_truths["nb_variants"] = len(markers)
+            # The first row contains the samples
+            samples = tuple(data[0])
+            nb_samples = len(samples)
+            if samples[0] == "None":
+                samples = None
+                nb_samples = int(data[0][1])
+            file_truths["samples"] = samples
+            file_truths["nb_samples"] = nb_samples
 
-    # Now, constructing the variants
-    cohort1_truths["variants"] = {}
-    for variant_data in data[1:]:
-        name, chrom, pos, a1, a2 = variant_data[:5]
-        variant = Variant(name, chrom, int(pos), a1, a2)
-        dosage = np.array(variant_data[5:], dtype=float)
-        cohort1_truths["variants"][name] = {
-            "variant": variant,
-            "dosage": dosage,
-        }
+            # The first column of each row contains the markers
+            markers = tuple(row[0] for row in data[1:])
+            file_truths["variant_set"] = set(markers)
+            file_truths["nb_variants"] = len(markers)
+
+            # Now, constructing the variants
+            file_truths["variants"] = {}
+            for variant_data in data[1:]:
+                name, chrom, pos, a1, a2 = variant_data[:5]
+                variant = Variant(name, chrom, int(pos), a1, a2)
+                dosage = np.array(variant_data[5:], dtype=float)
+                file_truths["variants"][name] = {
+                    "variant": variant,
+                    "dosage": dosage,
+                }
+
+        # Saving the file truths
+        final_truths[os.path.basename(fn)] = file_truths
+
+    return final_truths
+
+
+# All the truths
+truths = _generate_truths()
